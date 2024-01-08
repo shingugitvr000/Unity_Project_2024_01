412025562efd17e39241faf172b9d15cb639011c
shingugitvr000 <shingugitvr000@gmail.com>
2024-01-08 11:10:17 +0900

    큐브 이동 함수 추가

--- project_2024_01/Assets/Scripts/Temp_GameObject.cs
+++ project_2024_01/Assets/Scripts/Temp_GameObject.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Temp_GameObject : MonoBehaviour
 {
     public int hp = 30;                      //HP 변수 int 선언 
+    public float moveSpeed = 1.0f;           //이동 스피트 설정
     public Vector3 pos = Vector3.zero;       //Vector3 변수 선언 float
 
     void Start()
@@ -15,14 +16,30 @@ public class Temp_GameObject : MonoBehaviour
     }  
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))     //GetKeyDown키 값 함수 설정(키가 내려갔을때)
-        {
-            hp -= 1;                        //hp = hp - 1;
-        }
+        MoveCube();                                 //MoveCube 함수 사용 
 
-        if (Input.GetKey(KeyCode.UpArrow))     //GetKey키 값 함수 설정(키가 눌려져 있을때)
-        {   //Time.deltaTime 프레임 사이 시간을 알려주는 변수 
-            transform.position += new Vector3(0.0f, 1.0f * Time.deltaTime, 0.0f);
+        if (Input.GetKeyDown(KeyCode.Space))     //GetKeyDown키 값 함수 설정(키가 내려갔을때)
+        {
+            moveSpeed += 1;                       //스페이스 누를시 속도가 빨라짐 
+        }     
+    }
+    public void MoveCube()              //Cube 이동을 위한 함수 선언                    
+    {
+        if (Input.GetKey(KeyCode.UpArrow))     
+        {   //Vector3(X,Y,Z)
+            transform.position += new Vector3(0.0f, moveSpeed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))    
+        {  
+            transform.position += new Vector3(0.0f, -moveSpeed * Time.deltaTime, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))    
+        { 
+            transform.position += new Vector3(-moveSpeed * Time.deltaTime, 0.0f, 0.0f);
+        }
+        if (Input.GetKey(KeyCode.RightArrow))   
+        {  
+            transform.position += new Vector3(moveSpeed * Time.deltaTime, 0.0f, 0.0f);
         }
     }
 }
