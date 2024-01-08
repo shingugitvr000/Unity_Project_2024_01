db1ff87acec954c7cbbf25fa4906ab4648212d54
shingugitvr000 <shingugitvr000@gmail.com>
2024-01-08 14:09:37 +0900

    캐릭터 물리 이동

--- project_2024_01/Assets/Scripts/GameScprits/PlayerController.cs
+++ project_2024_01/Assets/Scripts/GameScprits/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public GameObject pivot;
     public Camera viewCamera;           //메인 카메라를 받아오는 Camera 오브젝트
     public Vector3 velocity;            //이동 값
+    public Rigidbody body;              //물리 효과를 주는 강체 값을 가져온다. 
    
     void Start()
     {
@@ -15,6 +16,9 @@ public class PlayerController : MonoBehaviour
     }    
     void Update()
     {
+        //방향키를 통해서 이동 벡터값을 생성한다. 
+        velocity = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized * moveSpeed;
+
         //화면에서 -> 게임 3D 공간 좌표를 변환해서 Vector3에 넣는다. 
         Vector3 mousePos = viewCamera.ScreenToWorldPoint(new Vector3(Input.mousePosition.x,
             Input.mousePosition.y, viewCamera.transform.position.y));
@@ -24,5 +28,9 @@ public class PlayerController : MonoBehaviour
 
         //피봇이 해당 타겟을 바라보게 한다. 
         pivot.transform.LookAt(targetPosition, Vector3.up);
+    }
+    private void FixedUpdate()
+    {
+        body.MovePosition(body.position + velocity * Time.fixedDeltaTime);
     }
 }
