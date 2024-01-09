3e97e5c8a64396feb931951a9e81f3c8650fcb23
shingugitvr000 <shingugitvr000@gmail.com>
2024-01-09 09:57:02 +0900

    Trigger 함수 사용

--- project_2024_01/Assets/Scripts/SphereCollision.cs
+++ project_2024_01/Assets/Scripts/SphereCollision.cs
@@ -4,6 +4,38 @@ using UnityEngine;
 
 public class SphereCollision : MonoBehaviour
 {
+    public int Hp = 100;
+    public float checkTime = 0.0f;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TRIGGER ENTER : " + other.gameObject.name);
+        if (other.gameObject.tag == "ITEM")
+        {
+            int cHP = other.gameObject.GetComponent<PlayerCollsion>().Hp;
+            Hp += cHP;
+            Destroy(other.gameObject);
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("TRIGGER EXIT : " + other.gameObject.name);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "FIELD")
+        {
+            checkTime += Time.deltaTime;
+            if (checkTime >= 1.0f)
+            {
+                Debug.Log("HP DOWN");
+                Hp -= 1;
+                checkTime = 0.0f;
+            }
+        }
+        Debug.Log("TRIGGER STAY : " + other.gameObject.name);
+    }
+
     //충돌에 진입 할 때
     private void OnCollisionEnter(Collision collision)
     {
@@ -12,15 +44,15 @@ public class SphereCollision : MonoBehaviour
         //    int cHP = collision.gameObject.GetComponent<PlayerCollsion>().Hp;
         //    Debug.Log("Collision HP : " + cHP);
         //}
-        Debug.Log("NAME ENTER : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
+        //Debug.Log("NAME ENTER : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
         //Debug.Log("TAG : " + collision.gameObject.tag);   //충돌체의 오브젝트 Tag를 가져온다.
     }
     private void OnCollisionStay(Collision collision)
     {
-        Debug.Log("NAME STAY : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
+        //Debug.Log("NAME STAY : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
     }
     private void OnCollisionExit(Collision collision)
     {
-        Debug.Log("NAME EXIT : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
+        // Debug.Log("NAME EXIT : " + collision.gameObject.name);   //충돌체의 오브젝트 이름을 가져온다.
     }
 }
