a4e0263cba091360d7b7e1704b05ce02e6dd93c9
shingugitvr000 <shingugitvr000@gmail.com>
2024-01-10 11:02:10 +0900

    UI 기본 테스트

--- project_2024_01/Assets/Scripts/GenSingleton.cs
+++ project_2024_01/Assets/Scripts/GenSingleton.cs
@@ -1,12 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;                       //UI를 사용하기위해서 선언
+using TMPro;
 
 public class GenSingleton : GenericSingleton<GenSingleton>
 {
+    //public Text textUI;                             //레거시 Text 선언
+    public TMP_Text tmptextUI;                      //TMP Text 선언
     public int playerScore = 0;
-    public void AddScore(int amount)
+    public int playerScoreMax = 100;
+    public Slider mainSlider;
+    public void AddScore(int amount)                //버튼 Event 와 연결 시키고 점수를 올린다. 
     {
         playerScore += amount;
+        tmptextUI.text = playerScore.ToString();        //Score 는 int 이기 때문에 ToString 로 문자열로 변환 
+        mainSlider.value = (float)playerScore / (float)playerScoreMax;  //Slider에 값을 반영할 때 
+    }
+
+    public void SubmitSliderValue()                 //Slider에서 값을 가져올 때
+    {
+        tmptextUI.text = mainSlider.value.ToString();
+    }
+
+    public void Start()
+    {      
+        tmptextUI.text = playerScore.ToString();
     }
 }
